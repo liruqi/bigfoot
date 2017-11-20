0dee9c96bf0c787f313d32e2fd7d3f0184fff098
liruqi <liruqi@gmail.com>
2017-11-20 21:06:03 +0800

    Update

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 rm -rfv Interface/AddOns/BigFootBank
 rm -rfv Interface/AddOns/TheBurningTrade
 rm -rfv Interface/AddOns/MySlot
+rm -rfv Interface/AddOns/TUnitFrame
 git add -A Interface
 git commit -m "Clean up"
 git push
