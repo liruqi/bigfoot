b0563e0e0ac6e9620400bf687ebc40b3aa94a734
root <liruqi@gmail.com>
2017-12-25 21:05:13 +0000

    Update clean list

--- clean.sh
+++ clean.sh
@@ -2,6 +2,8 @@ rm -rfv Interface/AddOns/BigFootBank
 rm -rfv Interface/AddOns/TheBurningTrade
 rm -rfv Interface/AddOns/MySlot
 rm -rfv Interface/AddOns/TUnitFrame
-git add -A Interface
+rm -rfv Interface/AddOns/BagBrother
+rm -rfv Interface/AddOns/Bagnon*
+git add -A Interface/AddOns/
 git commit -m "Clean up"
 git push
